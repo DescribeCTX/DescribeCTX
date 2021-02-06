2722274d25959333a33a43b5ad0b92e23dd2e8fe
DescribeCTX <77128721+DescribeCTX@users.noreply.github.com>
2021-02-06 17:23:54 -0500

    Add files via upload

---
+++ Code/ic3/runic3.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+appDir=$1
+forceAndroidJar=/Users/shaoyang/Library/Android/sdk/platforms/android-18/android.jar
+
+var=1
+for appPath in `ls $appDir/*.apk`
+do
+appName=`basename $appPath .apk`
+retargetedPath=testspace/$appName.apk
+
+mysql -uroot -pjiaozhuys05311 -e 'drop database if exists cc; create database cc'
+mysql -uroot -pjiaozhuys05311 cc < schema
+
+mkdir output/$appName
+
+gtimeout 1800 java -Xmx24000m -jar RetargetedApp.jar $forceAndroidJar $appPath $retargetedPath
+gtimeout 1800 java -Xmx24000m -jar ic3-0.2.0-full.jar -apkormanifest $appPath -input $retargetedPath -cp $forceAndroidJar -db cc.properties -dbname cc1 -protobuf output/$appName
+var=$((var+1))
+done
+echo "finished $var apk files."
